54cddf14c4b89280c3a0d72e2fe3c792daac28e3
nibblsch <32471930+nibblsch@users.noreply.github.com>
2025-05-16 15:36:43 -0400

    google auth works now, Stripe price table shows but you can't select one yet

--- src/routes/(marketing)/pricing/pricing_plans.ts
+++ src/routes/(marketing)/pricing/pricing_plans.ts
@@ -1,7 +1,7 @@
-export const defaultPlanId = "free"
+export const defaultPlanId = "pro"
 
 export const pricingPlans = [
-  {
+  /*{
     id: "free",
     name: "Free",
     description: "A free plan to get you started!",
@@ -9,7 +9,7 @@ export const pricingPlans = [
     priceIntervalName: "per month",
     stripe_price_id: null,
     features: ["MIT Licence", "Fast Performance", "Stripe Integration"],
-  },
+  },*/
   {
     id: "pro",
     name: "Pro",
@@ -17,8 +17,8 @@ export const pricingPlans = [
       "A plan to test the purchase experience. Try buying this with the test credit card 4242424242424242.",
     price: "$5",
     priceIntervalName: "per month",
-    stripe_price_id: "price_1NkdZCHMjzZ8mGZnRSjUm4yA",
-    stripe_product_id: "prod_OXj1CcemGMWOlU",
+    stripe_price_id: "price_1RP67uFL9lxuf6jZwVxGybDr",
+    stripe_product_id: "prod_SJjb3MSx3CfzCX",
     features: [
       "Everything in Free",
       "Support us with fake money",
@@ -32,8 +32,8 @@ export const pricingPlans = [
       "A plan to test the upgrade experience. Try buying this with the test credit card 4242424242424242.",
     price: "$15",
     priceIntervalName: "per month",
-    stripe_price_id: "price_1Nkda2HMjzZ8mGZn4sKvbDAV",
-    stripe_product_id: "prod_OXj20YNpHYOXi7",
+    stripe_price_id: "price_1RP6prFL9lxuf6jZ2Lbraa7I",
+    stripe_product_id: "prod_SJkKV3DZWTmttb",
     features: [
       "Everything in Pro",
       "Try the 'upgrade plan' UX",
